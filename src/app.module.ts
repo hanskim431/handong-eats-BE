6e93f5af7e71938aa697cebbe2fa79cf51a23b13
kimhans978 <fogy431@gmail.com>
2024-09-19 00:49:52 +0900

    🔧 (db): Update modules for Mongoose setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { MongooseModule } from '@nestjs/mongoose';
-
-const mongoUri = process.env.MONGODB_URI;
-
-if (!mongoUri) {
-  throw new Error('MONGODB_URI environment variable is not defined');
-}
 
 @Module({
-  imports: [
-    UsersModule,
-    MongooseModule.forRoot(mongoUri), //
-  ],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
