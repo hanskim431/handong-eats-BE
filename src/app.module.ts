d8f0e887a0ee1c90535c90aa8bb16bc99f0ef51d
hanskim431 <95575606+hanskim431@users.noreply.github.com>
2024-09-18 22:04:37 +0900

    ✨ (config): Add dotenv configuration and MongooseModule setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const mongoUri = process.env.MONGODB_URI;
+
+if (!mongoUri) {
+  throw new Error('MONGODB_URI environment variable is not defined');
+}
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(mongoUri), //
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
