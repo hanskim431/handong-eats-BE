2371b14e2f53f221a85f3a013408b2721a8b73dc
kimhans978 <fogy431@gmail.com>
2024-09-19 17:33:27 +0900

    🔧 (db): Update MongoDB connection string with credentials

--- database/database.provider.ts
+++ database/database.provider.ts
@@ -1,10 +1,17 @@
 import * as mongoose from 'mongoose';
 import 'dotenv/config';
 
-const mongodbUrl = process.env.MONGODB_URI;
+const mongodbID = process.env.MONGODB_ID;
+const mongodbPassword = process.env.MONGODB_PW;
+const collectionName = 'handong_eats';
+const mongodbUrl = `mongodb+srv://${mongodbID}:${mongodbPassword}@handong-eats.tkatv.mongodb.net/${collectionName}?retryWrites=true&w=majority&appName=handong-eats`;
 
-if (!mongodbUrl || typeof mongodbUrl !== 'string') {
-  throw new Error('MONGODB_URI 환경 변수를 확인해주세요.');
+if (!mongodbID || typeof mongodbID !== 'string') {
+  throw new Error('MONGODB_ID 환경 변수를 확인해주세요.');
+}
+
+if (!mongodbPassword || typeof mongodbPassword !== 'string') {
+  throw new Error('MONGODB_PW 환경 변수를 확인해주세요.');
 }
 
 export const databaseProviders = [
