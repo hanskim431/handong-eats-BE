0e93c45529a162258544a01db7a06aaa00c80313
hanskim431 <fogy431@gmail.com>
2024-09-20 22:37:28 +0900

    🐛 (fix): Resolve function duplication error

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,9 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { Model } from 'mongoose';
 import { User } from './interface/user.interface';
-import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UsersService {
@@ -17,10 +17,6 @@ export class UsersService {
 
   findAll() {
     return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
