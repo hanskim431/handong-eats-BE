7dc1f02bb6fd5f6a2bdfa30148d4da2c7f1ba79b
hanskim431 <95575606+hanskim431@users.noreply.github.com>
2024-09-23 00:02:47 +0900

    🐛 (fix): Correct issue with saving all properties in CreateUserDto

--- src/users/schema/users.schema.ts
+++ src/users/schema/users.schema.ts
@@ -1,8 +1,11 @@
 import * as mongoose from 'mongoose';
 
 export const UsersSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  gender: String,
-  phoneNumber: String,
+  userID: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
+  age: { type: Number, required: true },
+  phone: { type: String, required: true },
 });
