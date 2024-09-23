adda4210a3c44e280c7aa304c38fbf5da841afb5
hanskim431 <95575606+hanskim431@users.noreply.github.com>
2024-09-23 17:31:17 +0900

    🔧 (lint): Add eslint disable rule for unused variables

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -48,6 +48,7 @@ export class AuthService {
       });
 
     if (user && (await bcrypt.compare(pass, user.password))) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
       const { password, ...result } = user;
       return result;
     }
