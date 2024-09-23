5fc9341888f1e38417c73264010e9d040b6bf6ea
hanskim431 <95575606+hanskim431@users.noreply.github.com>
2024-09-23 11:47:55 +0900

    🔧 (db): Refactor MongoDB connection to use process.env directly

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,29 @@
 import { HttpException, HttpStatus, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
-import 'dotenv/config';
+import { UsersModule } from './users/users.module';
 
-const mongodb_id = process.env.MONGODB_ID;
-const mongodb_pw = process.env.MONGODB_PW;
-const mongodb_collection = process.env.MONGODB_COLLECTION;
-
-if (!mongodb_id || typeof mongodb_id != 'string') {
+if (!process.env.MONGODB_ID || typeof process.env.MONGODB_ID != 'string') {
   throw new HttpException(
     'MongoDB 환경 변수 확인 바람',
     HttpStatus.INTERNAL_SERVER_ERROR,
   );
 }
 
-if (!mongodb_pw || typeof mongodb_pw != 'string') {
+if (!process.env.MONGODB_PW || typeof process.env.MONGODB_PW != 'string') {
   throw new HttpException(
     'MongoDB 환경 변수 확인 바람',
     HttpStatus.INTERNAL_SERVER_ERROR,
   );
 }
 
-if (!mongodb_collection || typeof mongodb_collection != 'string') {
+if (
+  !process.env.MONGODB_COLLECTION ||
+  typeof process.env.MONGODB_COLLECTION != 'string'
+) {
   throw new HttpException(
     'MongoDB 환경 변수 확인 바람',
     HttpStatus.INTERNAL_SERVER_ERROR,
@@ -34,7 +33,7 @@ if (!mongodb_collection || typeof mongodb_collection != 'string') {
 @Module({
   imports: [
     MongooseModule.forRoot(
-      `mongodb+srv://${mongodb_id}:${mongodb_pw}@handong-eats.tkatv.mongodb.net/${mongodb_collection}?retryWrites=true&w=majority&appName=handong-eats-eats`,
+      `mongodb+srv://${process.env.MONGODB_ID}:${process.env.MONGODB_PW}@handong-eats.tkatv.mongodb.net/${process.env.MONGODB_COLLECTION}?retryWrites=true&w=majority&appName=handong-eats-eats`,
     ), // MongoDB 연결 설정
     UsersModule,
     AuthModule,
