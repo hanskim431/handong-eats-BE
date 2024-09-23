bf7e74a53881cdc426064bdd349fa3297c4b6720
hanskim431 <95575606+hanskim431@users.noreply.github.com>
2024-09-23 11:46:58 +0900

    🐛 (fix): Resolve issue with missing secretOrPrivateKey for JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,23 @@
-import { Module } from '@nestjs/common';
+import { HttpException, HttpStatus, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
+if (!process.env.JWT_SECRET || typeof process.env.JWT_SECRET !== 'string') {
+  throw new HttpException(
+    'JWT_SECRET 환경 변수 확인 바람',
+    HttpStatus.INTERNAL_SERVER_ERROR,
+  );
+}
 @Module({
-  imports: [JwtModule, UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET, // JWT 서명에 사용할 비밀키
+      signOptions: { expiresIn: '60s' }, // 토큰 만료 시간 설정
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
