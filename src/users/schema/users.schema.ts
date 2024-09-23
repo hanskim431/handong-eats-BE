d67c9e97217203ef400fe39a927361d108dee0fa
hanskim431 <95575606+hanskim431@users.noreply.github.com>
2024-09-23 19:56:39 +0900

    🔧 (users): Update user schema

--- src/users/schema/users.schema.ts
+++ src/users/schema/users.schema.ts
@@ -5,8 +5,9 @@ export const UsersSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  role: { type: String, required: true },
+  role: { type: String, required: true, default: 'consumer' },
   age: { type: Number, required: true },
   phone: { type: String, required: true },
+  point: { type: Number, required: true, default: 0 },
   refreshToken: { type: String, required: false },
 });
