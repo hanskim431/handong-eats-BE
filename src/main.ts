3bae57af706b2fc38b1588b6975034f6b4abb88f
hanskim431 <95575606+hanskim431@users.noreply.github.com>
2024-10-09 21:08:17 +0900

    (HTTP) cors 추가

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // CORS 설정 추가
+  app.enableCors(); // 모든 도메인에서 접근을 허용 (필요시 설정 가능)
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
